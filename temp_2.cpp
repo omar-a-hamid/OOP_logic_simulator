464784a8a537c27b5dfed21790688b2cdf9c59e2
Omar Ahmed <omarahmedabdelhamid@outlook.com>
2022-09-27 04:22:24 +0200

    parsing

--- temp_2.cpp
+++ temp_2.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <string.h>
+#include <string>
 #include <assert.h>
 // #include <>
 
@@ -26,11 +26,11 @@ class Node {
 
     public: 
 
-        Node(bool data=0, string name="N/A"){        
+        Node(bool data=0, char name='0'){        
             this->data=data;
             this->name=name;
         }
-        void set_name(string name){//virtual
+        void set_name(char name){//virtual
             this->name = name;
         }
         void set_data(bool data){
@@ -39,7 +39,7 @@ class Node {
         bool get_data(void){        
             return this->data;
         }
-        string get_name(void){          
+        char get_name(void){          
             return this->name;
         }
         static bool AND_(Node* A,Node* B){//pure virtual?
@@ -60,7 +60,7 @@ class Node {
     friend ostream& operator<<(ostream& out, const Node& node);
 
     private: 
-        string name;
+        char name;
         bool data;//bool
 
     protected:  
@@ -256,13 +256,13 @@ class Simulator /*private Gate*///singelton
             gates.push_back(gate);
         }
        
-        Node& FindNode(string node_name){
+        Node* FindNode(char node_name){
 
             for(auto const& node : nodes){
                 
                 if(node_name == node->get_name()){
 
-                    return  *node;//TODO: check that
+                    return  node;//TODO: check that
 
 
                 }
@@ -299,6 +299,88 @@ class GateGeneratortor: private Simulator{
 
     public: 
         void parseinput(void){
+            static string stdin_string;
+            char gate_1,gate_2,gate_3;
+            while(cin){
+
+                getline(cin, stdin_string);
+                string delimiter = " ";
+                
+                string command = stdin_string.substr(0, stdin_string.find(delimiter));
+                string command_data = stdin_string.substr(stdin_string.find(delimiter)+1 );
+                // cout << command<<endl;
+                
+                if(command=="AND"){
+                    /*
+                    check for nodes if the dont exsist, create nodes  
+                    */
+                    
+                    Gate* gate_ptr = createGate(AND);
+                    Node* node_i1 = createNode();
+                    node_i1->set_name(command_data[0]);
+                    Node* node_i2 = createNode();
+                    node_i2->set_name(command_data[2]);
+
+                    Node* node_o = createNode();
+                    node_o->set_name(command_data[4]);
+
+
+                    gate_ptr->set_input_1(node_i1);
+                    gate_ptr->set_input_2(node_i2);
+                    gate_ptr->set_output(node_o);
+
+
+
+
+                    cout<<
+                    command_data[0]
+                    <<endl;
+                    cout<<
+                    command_data[2]
+                    <<endl;
+                    cout<<
+                    command_data[4]
+                    <<endl;
+                    // cout<<"and" <<endl;
+
+
+
+                }else if(command=="NAND"){
+                    // cout<<"nand" <<endl;
+                    // while(cin>>gate_1>>gate_2>>gate_3);
+
+                }else if(command=="OR"){
+                    // cout<<"or" <<endl;
+                    
+                }else if(command=="NOR"){
+                    // cout<<"nor" <<endl;
+                    
+                }else if(command=="XOR"){
+                    // cout<<"xor" <<endl;
+                    
+                }else if(command=="XNOR"){
+                    // cout<<"xnor" <<endl;
+                    
+                }else if(command=="NOT"){
+                    // cout<<"not" <<endl;
+                    
+                }else if(command=="SET"){
+                    // cout<<"set" <<endl;
+                    
+                }else if(command=="SIM"){
+                    // cout<<"sim" <<endl;
+                    
+
+                }else if(command=="GET"){
+                    // cout<<"get" <<endl;
+                }else if(command=="OUT"){
+                    // cout<<"out" <<endl;
+                }
+
+            
+            }
+
+            
 
         }
     
@@ -313,7 +395,8 @@ class GateGeneratortor: private Simulator{
             return ((new Node()));
         
         }
-    
+    private: 
+
 
 };
 
@@ -322,8 +405,10 @@ int main() {
     Simulator simulator();
     GateGeneratortor gen;
 
+    gen.parseinput();
+
     // Node* node_i1 = (gen.createNode());
-    Node* node_i1= new Node(0,"A");
+/*    Node* node_i1= new Node(0,"A");
     Node* node_i2 = (gen.createNode());
     Node* node_o = (gen.createNode());
 
@@ -367,7 +452,7 @@ int main() {
     // cout << "fetch data: "<<node_o->get_data() <<endl;
     cout << *node_o<<endl;
 
-
+*/
     // cout<<node_o->get_data()<<endl;
 
 
